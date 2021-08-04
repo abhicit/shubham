201b34809c67d819c15f0b91e06df6bb010d35d7
Abhishek Dubey <niveus@DESKTOP-T5BGMI7>
2021-08-04 21:03:16 +0530

    method with return type

--- Shubham/src/com/shubham/basic/MethodPracticeWithReturnType.java
+++ Shubham/src/com/shubham/basic/MethodPracticeWithReturnType.java
@@ -4,8 +4,16 @@ public class MethodPracticeWithReturnType {
 	public static void main(String[] args) {
 		String firstName = "Shubham";
 		String lastName = "Jha";
+		float cgpa = 8.8f;
 		String fullName = getName(firstName, lastName);
+		float result = getPercentage(cgpa);
 		System.out.println(fullName);
+		System.out.println(result);
+	}
+	
+	public static float getPercentage(float cgpa) {
+		float result = cgpa * 10;
+		return result;
 	}
 	
 	public static String getName(String firstName, String lastName) {//Shubham Jha
